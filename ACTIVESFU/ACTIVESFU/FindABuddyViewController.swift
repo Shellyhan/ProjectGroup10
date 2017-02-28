b2a53f2404ba6dd8b28270fbded075debf17b3ab
bronwynbiro <bronwyn.biro@gmail.com>
2017-02-27 21:43:00 -0800

    added images and setup UI transitions, setup menu and login UI

--- ACTIVESFU/ACTIVESFU/FindABuddyViewController.swift
+++ ACTIVESFU/ACTIVESFU/FindABuddyViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FindABuddyViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ ACTIVESFU/ACTIVESFU/LoginViewController.swift
@@ -0,0 +1,38 @@
+//
+//  LoginViewController.swift
+//  ACTIVESFU
+//
+//  Created by Bronwyn Biro on 2017-02-27.
+//  Copyright © 2017 CMPT276 Group 10. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
