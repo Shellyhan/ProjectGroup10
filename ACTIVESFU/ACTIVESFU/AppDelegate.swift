3b3bb68b97cbff3b3f21c9a9434b0bf267577b4b
bronwynbiro <bronwyn.biro@gmail.com>
2017-03-01 16:29:21 -0800

    added firebase/cocoapods

--- ACTIVESFU/ACTIVESFU/AppDelegate.swift
+++ ACTIVESFU/ACTIVESFU/AppDelegate.swift
@@ -7,11 +7,21 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    
+    //Configure firebase
+    func application(application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?)
+            -> Bool {
+                FIRApp.configure()
+                return true
+        }
+
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
@@ -40,7 +50,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
+    
 
 }
 
---
+++ ACTIVESFU/Pods/Target Support Files/Pods-ACTIVESFU/Pods-ACTIVESFU-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_ACTIVESFUVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_ACTIVESFUVersionString[];
+
