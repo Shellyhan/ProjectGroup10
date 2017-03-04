00bf89381a47e091a8336f92771b63487ef1efe0
nca45 <nca45@sfu.ca>
2017-03-04 03:12:45 -0800

    Fixed signal sigabrt crashing, removed extra .plist file
    Fixed the outlet connections and missing files that was causing a
    Thread 1: Signal Sigabrt crash.
    
    Removed the necessity for googleservice-info-3.plist and only use
    googleservice-info.plist

--- ACTIVESFU/ACTIVESFU/LoginViewController.swift
+++ ACTIVESFU/ACTIVESFU/LoginViewController.swift
@@ -9,17 +9,14 @@
 import UIKit
 import Firebase
 
-class LoginViewController: UIViewController {
-    @IBOutlet weak var loginButton: UIButton!
-    @IBOutlet weak var usernameTextField: UITextField!
+class LoginViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
-    
-    //reference to the link to Frebase DB:
-    var ref: FIRDatabaseReference!
-    
-    
-    override func viewDidLoad() {
+
+       override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
 
@@ -27,69 +24,72 @@ class LoginViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    //MARK: Actions
-    @IBAction func createAccountButton(_ sender: UIButton) {
-        print("Create account was pressed")
-        
-        //looking for entering:
-        //let email = emailTextField.text
-        //let passward  = passwardTextField.text
-        //let username = nameTextField.text
-        let email: String = "111@gmail.com"
-        let passward: String = "123456"
-        let username: String = "Shelly"
-        
-        //Create new authentication and insert to DB:
-        FIRAuth.auth()?.createUser(withEmail: email, password: passward, completion: { (user: FIRUser?, error) in
-            if error != nil {
-            print("error")
-            return
-            }
-            guard let uid = user?.uid else{return}
+  
+    @IBAction func loginButton(_ sender: UIButton) {
+        if (self.emailTextField.text=="" || self.passwordTextField.text==""){
+            let alertController = UIAlertController(title: "Oops!", message: "Please enter and email and password.", preferredStyle: .alert)
+            let defaulAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaulAction)
             
-            //refernece to this user
-            self.ref = FIRDatabase.database().reference()
-            let UsersRef = self.ref.child("Users").child(uid)
-            
-            //insert user
-            UsersRef.updateChildValues(["user": username, "email": email], withCompletionBlock: { (err, ref) in
-                if err != nil {
-                    print("error")
-                    return
+            self.present(alertController, animated: true, completion: nil)
+        }
+        else
+        {
+            FIRAuth.auth()?.signIn(withEmail: self.emailTextField.text!, password: self.passwordTextField.text!, completion: { (user,error) in
+                if error == nil
+                    
+                {
+                    self.emailTextField.text=""
+                    self.passwordTextField.text=""
+                }
+                else
+                {
+                    let alertController = UIAlertController(title: "Oops!", message: error?.localizedDescription, preferredStyle: .alert)
+                    let defaulAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    alertController.addAction(defaulAction)
+                    
+                    self.present(alertController, animated: true, completion: nil)
+                    
                 }
             })
-        })
+            
+        }
+        
+        
         
     }
-    @IBAction func loginButton(_ sender: UIButton) {
-        print("Login was pressed")
+    @IBAction func accountButton(_ sender: UIButton) {
+        if (self.emailTextField.text=="" || self.passwordTextField.text==""){
+            let alertController = UIAlertController(title: "Oops!", message: "Please enter and email and password.", preferredStyle: .alert)
+            let defaulAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaulAction)
+            
+            
+            self.present(alertController, animated: true, completion: nil)
+        }
+        else {
+            FIRAuth.auth()?.signIn(withEmail: self.emailTextField.text!, password: self.passwordTextField.text!, completion: { (user,error) in
+                
+                if error == nil
+                    
+                {
+                    self.emailTextField.text=""
+                    self.passwordTextField.text=""
+                }
+                else
+                {
+                    let alertController = UIAlertController(title: "Oops!", message: error?.localizedDescription, preferredStyle: .alert)
+                    let defaulAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    alertController.addAction(defaulAction)
+                    
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            })
+        }
         
-        //looking for entering:
-        //let email = emailTextField.text
-        //let passward  = passwardTextField.text
-        //let username = nameTextField.text
-        let email: String = "111@gmail.com"
-        let passward: String = "123456"
-        
-        //checking the authentication:
-        FIRAuth.auth()?.signIn(withEmail: email, password: passward, completion: { (user, error) in
-            if error != nil {
-                print("error")
-                return
-            }
-        })
+    }
     }
     
-    
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-}
+
+
