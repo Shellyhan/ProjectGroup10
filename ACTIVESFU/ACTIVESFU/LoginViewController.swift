ffefe87a4dcb964a182df6f0d74862437e04dd61
nca45 <nca45@sfu.ca>
2017-03-04 15:08:54 -0800

    Merge branch 'master' into LoginTesting
    # Conflicts:
    #	ACTIVESFU/ACTIVESFU/LoginViewController.swift

--- ACTIVESFU/ACTIVESFU/LoginViewController.swift
+++ ACTIVESFU/ACTIVESFU/LoginViewController.swift
@@ -68,6 +68,7 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         }
         
         
+<<<<<<< HEAD
         
     }
     @IBAction func accountButton(_ sender: UIButton) {
@@ -100,6 +101,65 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         }
         
     }
+=======
+        func handleRegister() {
+            //wait for user input
+            // guard let email = usernameTextField.text, let password = passwordTextField.text, let name = usernameTextField.text else {
+            let email:String = "333@gmail.com"
+            let password:String = "123456"
+            let name:String = "shelly"
+            let friends:Array = ["7PT0C9flfDM3RcZtdcHURzySlaJ2", "orQY3pNQxJa1h5RcBa1QrjKblQg2"]//dummy users
+        
+            FIRAuth.auth()?.createUser(withEmail: email, password: password, completion: { (user: FIRUser?, error) in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                guard let uid = user?.uid else{return}
+            
+            //refernece to this user
+                self.ref = FIRDatabase.database().reference()
+                let UsersRef = self.ref.child("Users").child(uid)
+            
+            //insert user
+                UsersRef.updateChildValues(["user": name, "email": email, "contact": friends], withCompletionBlock: { (err, ref) in
+                    if err != nil {
+                        print(err!)
+                        return
+                    }
+                })
+            })
+        }
+        
+        handleRegister()
+        
+    }
+    @IBAction func loginButton(_ sender: UIButton) {
+        print("Login was pressed")
+        
+        func handleLogin() {
+            //wait for user input
+            let email: String = "333@gmail.com"
+            let password: String = "123456"
+            
+            //checking the authentication:
+            FIRAuth.auth()?.signIn(withEmail: email, password: password, completion: { (user, error) in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+            })
+        //show the user info
+            let uid = FIRAuth.auth()?.currentUser?.uid
+            self.ref = FIRDatabase.database().reference()
+            let UsersRef = self.ref.child("Users").child(uid!)
+            UsersRef.observeSingleEvent(of: .value, with: { (snapshot) in
+                print(snapshot)
+            }, withCancel: nil)
+        }
+
+        handleLogin()
+>>>>>>> master
     }
     
 
