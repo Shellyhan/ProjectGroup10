715a13bd7b5736aadb704d9403fda4c31de5d30e
Shelly Han <shellshell456@gmail.com>
2017-03-05 14:13:25 -0800

    date insertion

---
+++ ProjectGroup10-UpdateMonthandName/ACTIVESFU/ACTIVESFU/BuddiesViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ACTIVESFU
+//
+//  Created by Bronwyn Biro on 2017-02-03.
+//  Copyright © 2017 CMPT276 Group 10. All rights reserved.
+//
+
+import UIKit
+
+class BuddiesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
+
