0ec84df64869c4d7b729db13d7c9fd0e507db5d1
Nate Cheung <nca45@sfu.ca>
2017-03-22 13:37:37 -0700

    Formatting

--- ACTIVESFU/ACTIVESFU/BookFacilityViewController.swift
+++ ACTIVESFU/ACTIVESFU/BookFacilityViewController.swift
@@ -57,7 +57,6 @@ class BookFacilityViewController: UIViewController {
         
         let bookFacilityURLRequest = URLRequest(url: bookFacilityURL)
         
-
         bookFacilityWebView.loadRequest(bookFacilityURLRequest)
         
         bookFacilityWebView.scalesPageToFit = true
@@ -71,16 +70,4 @@ class BookFacilityViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
